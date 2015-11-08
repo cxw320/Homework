de708d3b67fabd2f96e4981084e25bd6aa5c9f3f
cxw320 <carolinewang@Carolines-MacBook-Air.local>
2015-11-07 19:04:55 -0500

    update

--- 10 Day - HTML of final Proj/Final Project/ResuMaker/form.js
+++ 10 Day - HTML of final Proj/Final Project/ResuMaker/form.js
@@ -65,29 +65,41 @@ $(document).ready(function(){
  		$('#skill8').text(inputSkill8);	
 
 
- 		var inputJobTitle1 = $('#inputJobTitle1').val();
+ 		var inputCompany1 = $('#inputCompany1').val();
+ 		console.log(inputCompany1);
+ 		$('#company1').text(inputCompany1);	
+
+   		var inputJobTitle1 = $('#inputJobTitle1').val();
  		console.log(inputJobTitle1);
- 		$('#iJobTitle1').text(inputJobTitle1);	
+ 		$('#JobTitle1').text(inputJobTitle1);	
 
-   		var inputJobDesc1 = $('#inputJobDesc1').val();
- 		console.log(inputJobDesc1);
- 		$('#inputJobDesc1').text(inputJobDesc1);	
+ 		var inputJobDesc1B1 = $('#inputJobDesc1B1').val();
+ 		console.log(inputJobDesc1B1);
+ 		$('#JobDesc1B1').text(inputJobDesc1B1 );
 
- 		var inputJobTitle2 = $('#inputJobTitle2').val();
- 		console.log(inputJobTitle2);
- 		$('#JobTitle2').text(inputJobTitle2);	
 
-   		var inputJobDesc2 = $('#inputJobDesc2').val();
- 		console.log(inputJobDesc2);
- 		$('#inputJobDesc2').text(inputJobDesc2);	
+ 		var inputJobDesc1B2 = $('#inputJobDesc1B2').val();
+ 		console.log(inputJobDesc1B2);
+ 		$('#JobDesc1B2').text(inputJobDesc1B2 );
 
- 		var inputJobTitle3 = $('#inputJobTitle3').val();
- 		console.log(inputJobTitle3);
- 		$('#inputJobTitle3').text(inputJobTitle3);	
 
-   		var inputJobDesc3 = $('#inputJobDesc3').val();
- 		console.log(inputJobDesc3);
- 		$('#inputJobDesc3').text(inputJobDesc3);
+ 		var inputJobDesc1B3 = $('#inputJobDesc1B3').val();
+ 		console.log(inputJobDesc1B3);
+ 		$('#JobDesc1B3').text(inputJobDesc1B3);	
+
+
+ 		var inputJobDesc1B4 = $('#inputJobDesc1B4').val();
+ 		console.log(inputJobDesc1B4);
+ 		$('#JobDesc1B4').text(inputJobDesc1B4);	
+
+ 		var inputJobDesc1B5 = $('#inputJobDesc1B5').val();
+ 		console.log(inputJobDesc1B5);
+ 		$('#JobDesc1B5').text(inputJobDesc1B5);
+
+ 		 var inputJobDesc1B6 = $('#inputJobDesc1B6').val();
+ 		console.log(inputJobDesc1B6);
+ 		$('#JobDesc1B6').text(inputJobDesc1B6);	
+
 
  	});
 
